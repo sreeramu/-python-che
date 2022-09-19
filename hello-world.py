544813d2ece7c61f3f9805a05b510d17bba2764c
Sreenivasan K <sreeramu@gmail.com>
2022-09-19 21:30:51 +0530

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,9 @@
+#--------------------------------------------------------------------------------------------
+#  Copyright (c) Red Hat, Inc. All rights reserved.
+#  Licensed under the MIT License. See LICENSE in the project root for license information.
+#--------------------------------------------------------------------------------------------
+
+# This program prints Hello, world!
+
+str = 'world'
+print('Hello, ' + str + '!')
